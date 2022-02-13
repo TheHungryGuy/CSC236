fbe3385a7fd39941074aa675b677ba338702040a
TheHungryGuy <60024274+TheHungryGuy@users.noreply.github.com>
2022-02-13 17:20:55 -0500

    Breaker class successful but missing dictionary to check

--- CSC236/src/caesarCipher/Breaker.java
+++ CSC236/src/caesarCipher/Breaker.java
@@ -4,52 +4,50 @@ package caesarCipher;
 public class Breaker {
 	
 	private final int maxShiftKey = 4;
+	private final int maxModKey = maxShiftKey -1;
 	private int shiftKey = 0;
 	private int modKey = 0;
-	int modKeyCount = 0;
+	private int modKeyCount = 0;
+	
 	
 	
 	public void Break(String encryptedText) {
-		//encryptedText is the String to be decrypted
 		
-		String decryptText = null;
-		char[]encryptArr = encryptedText.toCharArray();		//convert plainText into char array
-		shiftKey = 0;										//Resets shiftKey to 0
+		for(int k =0; k <= maxShiftKey;k++) {
 		
-		for(int j=1; j<=maxShiftKey; j++) {						//run for loop until go past the max
-			shiftKey++;											//increase the shiftKey
-			modKey = shiftKey;									//Sets the modKey to be just 1 under the shift key
-			int modMax = modKey -1; 							//Saves the max that modKey can be on this loop
-			for(int k=0; k<= modMax;k++) {						//for loop to run thru each modKey
-				modKey--;											//Reduces modKey by 1
-				modKeyCount = 0;								//Set the count to the modKey
-				encryptArr = encryptedText.toCharArray();			//resets Array 
+			for(int j =0; j <= maxModKey; j++ ) {
 				
-				for(int i=0; i<encryptedText.length();i++) {		//run for loop till last letter of plainText
-					if(Character.isLetter(encryptedText.charAt(i))) {			//checks if character is a letter if not skips
-						encryptArr[i] = (char) (encryptArr[i]- shiftKey);		 //shift letters in using shiftKey
-						encryptArr[i] = (char) (encryptArr[i]- modKeyCount);
-						if(modKeyCount <= modKey) {									//if statement to subtract in modKey
+				modKeyCount =0;
+				char[]encryptArr = encryptedText.toCharArray();		//convert plainText into char array
+				for(int i=0;i<encryptedText.length();i++) {			//run for loop till last letter of plainText
+					
+					if(Character.isLetter(encryptedText.charAt(i))) { //checks if character is a letter if not skips
+						encryptArr[i] = (char) (encryptArr[i]- shiftKey); //shift letters in using shiftKey
+						encryptArr[i] = (char) (encryptArr[i]- modKeyCount); //shift letters in using modKey
+						modKeyCount++; 		//Add 1 to the count so on next loop adds an extra
+						if(modKeyCount > modKey) { 	//when the modKeyCount goes over modKey
+							modKeyCount = 0;  //modKeyCount gets reset back to zero
 									
-							modKeyCount++; 												//Add 1 to the count so on next loop adds an extra
-							//System.out.println(modKeyCount);
-							if(modKeyCount >= modMax ) {
-								modKeyCount = 0;									// Resets modKeyCount back to current max
-								//System.out.println( "Reset mod keycount\t" + modKeyCount);
 							}
-						}	
-						//String decryptText = String.valueOf(encryptArr);		//Store decrypted text in string
-						//System.out.println(decryptText  + "\tShift Key-" + shiftKey +  "\tMod Key-"+ modKey); //Test print
-					}
-					//String decryptText = String.valueOf(encryptArr);		//Store decrypted text in string
-					//System.out.println(decryptText  + "\tShift Key-" + shiftKey +  "\tMod Key-"+ modKey); //Test print
+						}
 				}
-				 decryptText = String.valueOf(encryptArr);		//Store decrypted text in string
-				System.out.println(decryptText  + "\tShift Key-" + shiftKey +  "\tMod Key-"+ modKey); //Test print
-				
+		        String decryptedText = String.valueOf(encryptArr); //Store encryptedText
+		
+		        System.out.println(decryptedText  + " is the decrypted code of " + encryptedText +
+		        		" with a shift key of " + shiftKey + " and mod key of " + modKey); //print out to test
+			
+		        
+		        modKey++;
 			}
+		
+			shiftKey++;
+			modKey=0;
 		}
-	}	
+		
+		
+	}
+	
+	
 
 }
 //The Breaker class will contain the method to break the Caesar cipher.
