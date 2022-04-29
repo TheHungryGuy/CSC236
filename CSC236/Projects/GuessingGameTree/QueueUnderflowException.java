cc9941e05b191e99a5eb303b326fc6c61e98c03d
TheHungryGuy <60024274+TheHungryGuy@users.noreply.github.com>
2022-04-29 19:15:09 -0400

    Lab 4 Base Files

---
+++ CSC236/Projects/GuessingGameTree/QueueUnderflowException.java
@@ -0,0 +1,15 @@
+package GuessingGameTree;
+
+
+public class QueueUnderflowException extends RuntimeException
+{
+  public QueueUnderflowException()
+  {
+    super();
+  }
+
+  public QueueUnderflowException(String message)
+  {
+    super(message);
+  }
+}
---
+++ CSC236/Projects/GuessingGameTree/TreeViolationException.java
@@ -0,0 +1,10 @@
+package GuessingGameTree;
+
+public class TreeViolationException extends RuntimeException {
+    public TreeViolationException() {
+        super();
+    }
+    public TreeViolationException(String message) {
+        super(message);
+    }
+}
